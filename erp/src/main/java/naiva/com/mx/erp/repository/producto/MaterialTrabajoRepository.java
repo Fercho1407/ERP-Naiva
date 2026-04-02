26e0872f280fa4e87c1a7b998d05628ebe457bd3
Fernando Robles <luis.fer1407@gmail.com>
2026-04-02 09:45:13 -0600

    Creacion de los Repository para las entidades relacionadas a un producto

---
+++ erp/src/main/java/naiva/com/mx/erp/repository/producto/MaterialTrabajoRepository.java
@@ -0,0 +1,9 @@
+package naiva.com.mx.erp.repository.producto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialTrabajoRepository extends JpaRepository<MaterialTrabajoRepository, Integer>{
+    
+}
---
+++ erp/src/main/java/naiva/com/mx/erp/repository/producto/ProductoVarianteRepository.java
@@ -0,0 +1,7 @@
+package naiva.com.mx.erp.repository.producto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoVarianteRepository extends JpaRepository<ProductoVarianteRepository, Integer> {
+    
+}
