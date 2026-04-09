ddbacd94bb1a1b15a327d182425c1896609c7e45
fercho1407 <luis.fer1407@gmail.com>
2026-04-09 13:23:06 -0600

    Se agrega un endpoint que permite obtner los almacenes registrados en la base de datos, con su respectivo service

--- erp/src/main/java/naiva/com/mx/erp/controllers/AlmacenController.java
+++ erp/src/main/java/naiva/com/mx/erp/controllers/AlmacenController.java
@@ -1,5 +1,7 @@
 package naiva.com.mx.erp.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,26 +9,37 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import naiva.com.mx.erp.DTO.Creates.AlmacenCreateDTO;
 import naiva.com.mx.erp.DTO.Responses.AlmacenResponseDTO;
+import naiva.com.mx.erp.model.Almacen;
 import naiva.com.mx.erp.service.AlmacenService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
 @RequestMapping("naivaERP")
 @CrossOrigin("*")
+@RequiredArgsConstructor
 public class AlmacenController {
 
-    @Autowired private AlmacenService almacenService;
+    private final AlmacenService almacenService;
 
     @PostMapping("/almacen")
     public ResponseEntity<AlmacenResponseDTO> guardarAlmacen(@RequestBody AlmacenCreateDTO almacenCreateDTO) {
         AlmacenResponseDTO almacenGuardado = almacenService.guardarAlmacen(almacenCreateDTO);
         return new ResponseEntity<>(almacenGuardado, HttpStatus.CREATED);
     }
+
+    @GetMapping("/almacenes")
+    public ResponseEntity<List<Almacen>> getAlmacenes() {
+        return new ResponseEntity<>(almacenService.getListaAlmacenes(), HttpStatus.OK);
+    }
+    
     
     
 }
--- erp/src/main/java/naiva/com/mx/erp/service/AlmacenService.java
+++ erp/src/main/java/naiva/com/mx/erp/service/AlmacenService.java
@@ -1,8 +1,10 @@
 package naiva.com.mx.erp.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import naiva.com.mx.erp.DTO.Creates.AlmacenCreateDTO;
 import naiva.com.mx.erp.DTO.Creates.DireccionCreateDTO;
 import naiva.com.mx.erp.DTO.Responses.AlmacenResponseDTO;
@@ -11,12 +13,11 @@ import naiva.com.mx.erp.model.Direccion;
 import naiva.com.mx.erp.repository.AlmacenRepository;
 
 @Service
+@RequiredArgsConstructor
 public class AlmacenService {
     
-    @Autowired
-    private AlmacenRepository almacenRepository;
-    @Autowired 
-    private DireccionService direccionService;
+    private final AlmacenRepository almacenRepository;
+    private final DireccionService direccionService;
 
     /**
      * Recibe todos los datos necesarios para crear un almacen con su respectiva direccion
@@ -56,5 +57,21 @@ public class AlmacenService {
          * Retorna solamente lo necesario
          */
         return new AlmacenResponseDTO(almacen.getNombre());
+    }
+
+    /**
+     * Metodo que permite obtener todos los almacenes registrados en la base de datos
+     * @return Lista de alamcenes  
+     */
+    public List<Almacen> getListaAlmacenes(){
+        var almacenes = almacenRepository.findAll();
+
+        if(almacenes.isEmpty()) throw new RuntimeException("No hay almacenes registrados");
+
+        for(Almacen almacen: almacenes){
+            almacen.setDireccion(null);
+        }
+
+        return almacenes;
     }
 }
